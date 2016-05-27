0bec422187db428ad8071960d92c637d0937eab4
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-05-26 23:48:18 -0700

    Turn contributions red
    This makes them look more like work

---
+++ change-text.js
@@ -0,0 +1,7 @@
+$('.contrib-legend span:eq(0)').text('Less Work');
+$('.contrib-legend span:eq(1)').text('More Work');
+$('.contrib-legend li:eq(0)').css('background-color', '#44a340');
+$('.contrib-legend li:eq(1)').css('background-color', 'red').css('opacity', .25);
+$('.contrib-legend li:eq(2)').css('background-color', 'red').css('opacity', .5);
+$('.contrib-legend li:eq(3)').css('background-color', 'red').css('opacity', .75);
+$('.contrib-legend li:eq(4)').css('background-color', 'red').css('opacity', 1);
