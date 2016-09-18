a8465a49555c5e2a7c3e2bf65cbc8e924df0ac10
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-09-18 09:18:14 -0700

    Update for the new Github profile page
    Changes to HTML structure necessitated a change to the extension.  I
    also made the colors a bit more pleasing to the eye.

--- change-text.js
+++ change-text.js
@@ -1,7 +1,10 @@
-$('.contrib-legend span:eq(0)').text('Less Work');
-$('.contrib-legend span:eq(1)').text('More Work');
-$('.contrib-legend li:eq(0)').css('background-color', '#44a340');
-$('.contrib-legend li:eq(1)').css('background-color', 'red').css('opacity', .25);
-$('.contrib-legend li:eq(2)').css('background-color', 'red').css('opacity', .5);
-$('.contrib-legend li:eq(3)').css('background-color', 'red').css('opacity', .75);
-$('.contrib-legend li:eq(4)').css('background-color', 'red').css('opacity', 1);
+var html = $('.contrib-legend').html();
+html = html.replace('Less', 'Less Work');
+html = html.replace('More', 'More Work')
+$('.contrib-legend').html(html);
+
+$('.contrib-legend li:eq(0)').css('background-color', '#090');
+$('.contrib-legend li:eq(1)').css('background-color', '#e00').css('opacity', .25);
+$('.contrib-legend li:eq(2)').css('background-color', '#e00').css('opacity', .5);
+$('.contrib-legend li:eq(3)').css('background-color', '#e00').css('opacity', .75);
+$('.contrib-legend li:eq(4)').css('background-color', '#e00').css('opacity', 1);
